9f0b90b85e52ea3eabb65e0491cbb0d834cf350d
sandman6094 <3399893920@qq.com>
2020-07-06 18:38:59 -0700

    Add files via upload

---
+++ mid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double a = -1;
+    double b = 1;
+    int n = 0;
+    double xi_1 = 0.0;
+    double xi = 0.0;
+    double overline_xi = 0.0;
+    double delta_x = 0.0;
+    double fsum = 0.0;
+    double sum = 0.0;
+    const double e = 2.71828182846;
+
+    printf("Please enter n.\n");
+    scanf("%d", &n);
+
+    delta_x = (b-a)/n;
+
+    for(int i=0; i < n; i++){
+        xi_1 = a + i * delta_x;
+        xi = a + (i+1) * delta_x;
+        overline_xi = 0.5 * (xi_1 + xi);
+        fsum += pow(e, -1 * pow(overline_xi, 2));
+    }
+   
+    sum = delta_x * fsum;
+    printf("fx 的和 = %f, delta X = %f, 最终结果 = %f \n", fsum, delta_x, sum);
+
+    return 0;
+}
---
+++ trap.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double a = -1.0;
+    double b = 1.0;
+    int n = 0;
+    double delta_x = 0.0;
+    double xi = 0.0;
+    double fsum = 0.0;
+    double sum = 0.0;
+    const double e = 2.71828182846;
+
+    printf("Please enter n.\n");
+    scanf("%d", &n);
+
+    delta_x = (b - a) / n;
+
+    for (int i = 0; i <= n; i++){
+        xi = a + i * delta_x;
+
+        if (i == 0){
+            fsum += pow(e, -1 * pow(xi, 2));
+        }
+        else if (i == n){
+            fsum += pow(e, -1 * pow(xi, 2));
+        }
+        else {
+            fsum += 2 * pow(e, -1 * pow(xi, 2));
+        }
+    }
+
+    sum = delta_x * 0.5 * fsum;
+
+    printf("fx的和 = %f, delta x = %f, 最终结果 = %f\n", fsum, delta_x, sum);
+
+    return 0;
+}
