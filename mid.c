a46bbab18a29763e62f2a137ba139b5533d3ecc6
sandman6094 <3399893920@qq.com>
2020-07-06 18:34:06 -0700

    Add files via upload

---
+++ mid.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double a = -1;
+    double b = 1;
+    int n = 0;
+    double xi_1 = 0.0;
+    double xi = 0.0;
+    double overline_xi = 0.0;
+    double delta_x = 0.0;
+    double sum = 0.0;
+    const double e = 2.71828182846;
+
+    printf("Please enter n.\n");
+    scanf("%d", &n);
+
+    delta_x = (b-a)/n;
+
+    for(int i=0; i < n; i++){
+        xi_1 = a + i * delta_x;
+        xi = a + (i+1) * delta_x;
+        overline_xi = 0.5 * (xi_1 + xi);
+        sum += pow(e, -1 * pow(overline_xi, 2));
+    }
+   
+    sum = delta_x * sum;
+    printf("%f\n",sum);
+
+    return 0;
+}
